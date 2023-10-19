f65eb39bb01bad39b3751da56205bc916bd6f84f
SahidCHairulf <sahidchairulf>
2023-10-19 17:27:09 +0800

    MAP

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="bg-black h-screen">
-        <iframe title="" aria-label="Map" id="datawrapper-chart-Uez0V" src="https://datawrapper.dwcdn.net/Uez0V/3/"
-            scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="520"
+        <iframe title="" aria-label="Map" id="datawrapper-chart-Uez0V" src="https://datawrapper.dwcdn.net/Uez0V/14/"
+            scrolling="no" frameborder="0" style="width: 0; height: 40%; min-width: 100% !important; border: none;"
             data-external="1"
-            class="w-auto h-screen bg-black items-center justify-center text-white">
-        </iframe>
+            class="h-screen w-auto justify-center items-center py-6"
+            ></iframe>
     </div>
 </template>
-<script type="text/javascript">
-!function () { "use strict"; window.addEventListener("message", (function (a) { if (void 0 !== a.data["datawrapper-height"]) { var e = document.querySelectorAll("iframe"); for (var t in a.data["datawrapper-height"]) for (var r = 0; r < e.length; r++)if (e[r].contentWindow === a.source) { var i = a.data["datawrapper-height"][t] + "px"; e[r].style.height = i } } })) }();
+<script type="text/javascript">!function () { "use strict"; window.addEventListener("message", (function (a) { if (void 0 !== a.data["datawrapper-height"]) { var e = document.querySelectorAll("iframe"); for (var t in a.data["datawrapper-height"]) for (var r = 0; r < e.length; r++)if (e[r].contentWindow === a.source) { var i = a.data["datawrapper-height"][t] + "px"; e[r].style.height = i } } })) }();
 </script>
