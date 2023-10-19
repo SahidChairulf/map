62b22ebb5d65870b0ed7b963047cc1e1c97d8a53
SahidCHairulf <sahidchairulf>
2023-10-19 14:11:17 +0800

    MAP

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,17 +1,9 @@
 <template>
-    <div class="grid grid-cols-2 gap-2 bg-black h-screen">
-        <div class="mt-24">
-            <h1 class="font-extrabold text-3xl text-center justify-center mt-32 text-white">
-            MAP KABUPATEN BANDUNG
-            </h1>
-            <h4 class="text-white justify-center ml-14 mt-2">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </h4>
-        </div>
+    <div class="bg-black h-screen">
         <iframe title="" aria-label="Map" id="datawrapper-chart-Uez0V" src="https://datawrapper.dwcdn.net/Uez0V/3/"
             scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="520"
             data-external="1"
-            class="w-auto h-screen bg-black mt-16 mr-12 items-center justify-center">
+            class="w-auto h-screen bg-black mt-16 mr-12 items-center justify-center text-white">
         </iframe>
     </div>
 </template>
